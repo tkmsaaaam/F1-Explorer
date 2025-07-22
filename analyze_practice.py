bb6788a558c33c53b55a8d2d969457e58376334d
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-07-22 21:03:17 +0900

    chore

--- analyze_practice.py
+++ analyze_practice.py
@@ -49,7 +49,7 @@ if config['Session'].startswith('FP'):
     ) + [session.laps.pick_fastest().get_telemetry().add_distance()['Distance'].iloc[-1]]
     short_runs.plot_mini_segment_on_circuit(session, log, corners, 'corners')
     short_runs.compute_and_save_segment_tables_plotly(session,
-                                                      f"./images/{session.event.year}/{session.event['RoundNumber']}_{session.event.Location}/{session.name.replace(' ', '')}/courners_table",
+                                                      f"./images/{session.event.year}/{session.event['RoundNumber']}_{session.event.Location}/{session.name.replace(' ', '')}/corners_table",
                                                       corners,
                                                       log)
     corner_map: dict[str: list[int]] = config["corners"]
