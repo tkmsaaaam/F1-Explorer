82f139becfa3ed28120a7d8cf9a89c90e015a1a5
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-09-15 20:43:11 +0900

    refactor

--- analyze_season.py
+++ analyze_season.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 from itertools import accumulate
@@ -17,12 +18,23 @@ log = logging.getLogger(__name__)
 season = 2025
 schedule = fastf1.get_event_schedule(season, include_testing=False)
 
+drivers = []
 standings = {}
 colors = {}
+
+# {"round_number": {"name": "Japan", "sprint": true, "sprint_position": {"abbreviation": 1},"grid_position": {"abbreviation": 1}, "position": {"abbreviation": 1}}}
+results = {}
 
 schedule = schedule.sort_values(by='RoundNumber')
 for _, event in schedule.iterrows():
     event_name, round_number = event.EventName, event.RoundNumber
+
+    if round_number not in results:
+        results[round_number] = {}
+
+    round = results[round_number]
+    round["name"] = event_name
+    round["date"] = event.EventDate
 
     race = fastf1.get_session(season, event_name, "R")
     race.load(laps=False, telemetry=False, weather=False, messages=False)
@@ -31,33 +43,63 @@ for _, event in schedule.iterrows():
     if event["EventFormat"] == "sprint_qualifying":
         sprint = fastf1.get_session(season, event_name, "S")
         sprint.load(laps=False, telemetry=False, weather=False, messages=False)
+        round["sprint"] = True
+        if "sprint_position" not in round:
+            round["sprint_position"] = {}
+        if "sprint_point" not in round:
+            round["sprint_point"] = {}
+        for _, driver_row in sprint.results.iterrows():
+            round["sprint_position"][driver_row.Abbreviation] = driver_row.Position
+            round["sprint_point"][driver_row.Abbreviation] = driver_row.Points
+    else:
+        round["sprint"] = False
 
+    if "grid_position" not in round:
+        round["grid_position"] = {}
+    if "position" not in round:
+        round["position"] = {}
+    if "point" not in round:
+        round["point"] = {}
     for _, driver_row in race.results.iterrows():
-        driver_number, race_points, abbreviation = (
-            driver_row.DriverNumber,
-            driver_row.Points,
-            driver_row.Abbreviation,
-        )
+        abbreviation = driver_row.Abbreviation
+        round["grid_position"][abbreviation] = driver_row.GridPosition
+        round["position"][abbreviation] = driver_row.Position
+        round["point"][abbreviation] = driver_row.Points
 
+        if abbreviation not in drivers:
+            drivers.append(abbreviation)
         if abbreviation not in colors:
             colors[abbreviation] = race.get_driver(abbreviation).TeamColor
 
-        sprint_points = 0
-        if sprint is not None:
-            sprint_points = sprint.results.Points.get(driver_number, 0)
+now = datetime.datetime.now()
+latest = 0
 
-        if abbreviation not in standings:
-            standings[abbreviation] = [0] * (round_number - 1)
-        standings[abbreviation].append(race_points + sprint_points)
-    for v in standings.values():
-        v.extend([0] * (round_number - len(v)))
+for i in range(1, 53):
+    if now < results[i]["date"]:
+        break
+    latest = i
 
 base_dir = f"./images/{season}"
 
 fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=150, layout='tight')
-for k, v in standings.items():
-    ax.plot([i for i in range(1, len(v) + 1)], [sum(v[:i + 1]) for i in range(len(v))], label=k,
-            color='#' + colors.get(k, '000000'), linewidth=1)
+champion_sum = 0
+champion = ""
+for v in drivers:
+    y = []
+    s = 0
+    for i in range(1, latest + 1):
+        p = results[i]["point"].get(v, 0)
+        if results[i]["sprint"]:
+            y.append(p + results[i]["sprint_point"].get(v, 0))
+            s += p + results[i]["sprint_point"].get(v, 0)
+        else:
+            y.append(p)
+            s += p
+    if s > champion_sum:
+        champion_sum = s
+        champion = v
+    ax.plot([i for i in range(1, latest + 1)], [sum(y[:i + 1]) for i in range(len(y))], label=v,
+            color='#' + colors.get(v, '000000'), linewidth=1)
 ax.legend(fontsize='small')
 ax.grid(True)
 output_path = f"{base_dir}/standings.png"
@@ -66,9 +108,18 @@ fig.savefig(output_path, bbox_inches='tight')
 plt.close(fig)
 log.info(f"Saved plot to {output_path}")
 fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=150, layout='tight')
-for k, v in standings.items():
-    x = [i for i in range(1, len(v) + 1)]
-    ax.plot(x, v, label=k, color='#' + colors.get(k, '000000'), linewidth=1)
+champion_points = []
+for v in drivers:
+    y = []
+    for i in range(1, latest + 1):
+        p = results[i]["point"].get(v, 0)
+        if results[i]["sprint"]:
+            y.append(p + results[i]["sprint_point"].get(v, 0))
+        else:
+            y.append(p)
+    ax.plot([i for i in range(1, latest + 1)], y, label=v, color='#' + colors.get(v, '000000'), linewidth=1)
+    if v == champion:
+        champion_points = y
 ax.legend(fontsize='small')
 ax.grid(True)
 output_path = f"{base_dir}/results.png"
@@ -77,18 +128,19 @@ fig.savefig(output_path, bbox_inches='tight')
 plt.close(fig)
 log.info(f"Saved plot to {output_path}")
 
-champion_number, champion_point = max(
-    ((k, sum(v)) for k, v in standings.items()),
-    key=lambda item: item[1]
-)
 fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=150, layout="tight")
-champion_sum = list(accumulate(standings[champion_number]))
-for k, v in standings.items():
-    if k == champion_number:
+for v in drivers:
+    if v == champion:
         continue
-    x = range(1, len(v) + 1)
-    diff = [a - b for a, b in zip(accumulate(v), champion_sum)]
-    ax.plot(x, diff, label=k, color="#" + colors.get(k, "000000"), linewidth=1)
+    y = []
+    for i in range(1, latest + 1):
+        p = results[i]["point"].get(v, 0)
+        if results[i]["sprint"]:
+            y.append(p + results[i]["sprint_point"].get(v, 0))
+        else:
+            y.append(p)
+    diff = [a - b for a, b in zip(accumulate(y), accumulate(champion_points))]
+    ax.plot([i for i in range(1, latest + 1)], diff, label=v, color="#" + colors.get(v, "000000"), linewidth=1)
 ax.legend(fontsize='small')
 ax.grid(True)
 output_path = f"{base_dir}/diffs.png"
@@ -97,29 +149,12 @@ fig.savefig(output_path, bbox_inches='tight')
 plt.close(fig)
 log.info(f"Saved plot to {output_path}")
 
-grid_positions = {}
-
-for _, event in schedule.iterrows():
-    event_name, round_number = event.EventName, event.RoundNumber
-    qualify = fastf1.get_session(season, event_name, "Q")
-    qualify.load(laps=False, telemetry=False, weather=False, messages=False)
-    for _, driver_row in qualify.results.iterrows():
-        position, abbreviation = (
-            driver_row.Position,
-            driver_row.Abbreviation,
-        )
-
-        if abbreviation not in grid_positions:
-            grid_positions[abbreviation] = [21] * (round_number - 1)
-        grid_positions[abbreviation].append(position)
-    for v in standings.values():
-        v.extend([21] * (round_number - len(v)))
-
 fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=150, layout='tight')
-for k, v in grid_positions.items():
-    log.info(f"{k}: {sum(v) / len(v)}")
-    ax.plot([i for i in range(1, len(v) + 1)], v, label=k,
-            color='#' + colors.get(k, '000000'), linewidth=1)
+for v in drivers:
+    y = []
+    for i in range(1, latest + 1):
+        y.append(results[i]["grid_position"].get(v, 21))
+    ax.plot([i for i in range(1, latest + 1)], y, label=v, color='#' + colors.get(v, '000000'), linewidth=1)
 ax.legend(fontsize='small')
 ax.grid(True)
 ax.invert_yaxis()
@@ -129,28 +164,12 @@ fig.savefig(output_path, bbox_inches='tight')
 plt.close(fig)
 log.info(f"Saved plot to {output_path}")
 
-results = {}
-
-for _, event in schedule.iterrows():
-    event_name, round_number = event.EventName, event.RoundNumber
-    race = fastf1.get_session(season, event_name, "R")
-    race.load(laps=False, telemetry=False, weather=False, messages=False)
-    for _, driver_row in race.results.iterrows():
-        position, grid_position, abbreviation = (
-            driver_row.Position,
-            driver_row.GridPosition,
-            driver_row.Abbreviation,
-        )
-
-        if abbreviation not in results:
-            results[abbreviation] = [0] * (round_number - 1)
-        results[abbreviation].append(grid_position - position)
-    for v in standings.values():
-        v.extend([] * (round_number - len(v)))
-
 fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=150, layout='tight')
-for k, v in results.items():
-    ax.plot([i for i in range(1, len(v) + 1)], v, label=k, color='#' + colors.get(k, '000000'), linewidth=1)
+for v in drivers:
+    y = []
+    for i in range(1, latest + 1):
+        y.append(results[i]["grid_position"].get(v, 21) - results[i]["position"].get(v, 21))
+    ax.plot([i for i in range(1, latest + 1)], y, label=v, color='#' + colors.get(v, '000000'), linewidth=1)
 ax.legend(fontsize='small')
 ax.grid(True)
 output_path = f"{base_dir}/grid_to_results.png"
