44cb1f6aadfa65c1167ddd39cb41d16d10744253
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-11-15 06:57:35 +0900

    feat: add a test case

--- visualizations/test_long_runs.py
+++ visualizations/test_long_runs.py
@@ -59,7 +59,7 @@ class LongRuns(unittest.TestCase):
         self.assertEqual(83.456, list(stint_set)[0].laps.get(1))
         self.assertEqual(83.000, list(stint_set)[0].laps.get(3))
 
-    def test_make_stint_set(self):
+    def test_make_stint_set_one_driver(self):
         data = {
             "DriverNumber": ["1", "1", "1"],
             "Driver": ["Max", "Max", "Max"],
@@ -81,6 +81,21 @@ class LongRuns(unittest.TestCase):
         self.assertEqual(83.456, list(stint_set)[0].laps.get(1))
         self.assertEqual(82.789, list(stint_set)[0].laps.get(2))
         self.assertEqual(83.000, list(stint_set)[0].laps.get(3))
+
+    def test_make_stint_set_two_drivers(self):
+        data = {
+            "DriverNumber": ["1", "1", "1", "16", "16"],
+            "Driver": ["Max", "Max", "Max", "Charles", "Charles"],
+            "Stint": [1, 1, 1, 1, 1],
+            "Team": ["Red Bull", "Red Bull", "Red Bull", "Ferrari", "Ferrari"],
+            "LapTime": pandas.to_timedelta(["83.456s", "82.789s", "83.000s", "83.600s", "83.700s"]),
+            "TyreLife": [1, 2, 3, 1, 2],
+            "Compound": ["SOFT", "SOFT", "SOFT", "SOFT", "SOFT"]
+        }
+
+        laps = Laps(pandas.DataFrame(data))
+        stint_set: set[Stint] = make_stint_set(2, laps, "SOFT")
+        self.assertEqual(2, len(stint_set))
 
 
 if __name__ == '__main__':
