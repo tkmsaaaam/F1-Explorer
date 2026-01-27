b312b523315edb46b1d3de4ae9899f1d60f1bd21
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2026-01-27 21:44:47 +0900

    refactor

--- constants.py
+++ constants.py
@@ -1,37 +1,4 @@
-compound_color: dict[str, str] = {
-    "HARD": "white",
-    "MEDIUM": "yellow",
-    "SOFT": "red",
-    "INTERMEDIATE": "green",
-    "WET": "blue"
-}
-
-team_color: dict[int, dict[int, str]] = {
-    2025: {
-        1: "#0600ef",
-        22: "#0600ef",
-        16: "#e80020",
-        44: "#e80020",
-        63: "#27f4d2",
-        12: "#27f4d2",
-        4: "#ff8000",
-        81: "#ff8000",
-        31: "#b6babd",
-        87: "#b6babd",
-        14: "#00665f",
-        18: "#00665f",
-        10: "#ff87bc",
-        7: "#ff87bc",
-        43: "#ff87bc",
-        6: "#fcd700",
-        30: "#fcd700",
-        5: "#00e700",
-        27: "#00e700",
-        23: "#00a0dd",
-        55: "#00a0dd",
-    }
-}
-
+# for backup
 abbreviation: dict[int, dict[int, str]] = {
     2025: {
         1: "VER",
@@ -81,5 +48,40 @@ camera: dict[int, dict[int, str]] = {
         27: "yellow",
         23: "black",
         55: "yellow"
+    }
+}
+
+compound_color: dict[str, str] = {
+    "HARD": "white",
+    "MEDIUM": "yellow",
+    "SOFT": "red",
+    "INTERMEDIATE": "green",
+    "WET": "blue"
+}
+
+# for backup
+team_color: dict[int, dict[int, str]] = {
+    2025: {
+        1: "#0600ef",
+        22: "#0600ef",
+        16: "#e80020",
+        44: "#e80020",
+        63: "#27f4d2",
+        12: "#27f4d2",
+        4: "#ff8000",
+        81: "#ff8000",
+        31: "#b6babd",
+        87: "#b6babd",
+        14: "#00665f",
+        18: "#00665f",
+        10: "#ff87bc",
+        7: "#ff87bc",
+        43: "#ff87bc",
+        6: "#fcd700",
+        30: "#fcd700",
+        5: "#00e700",
+        27: "#00e700",
+        23: "#00a0dd",
+        55: "#00a0dd",
     }
 }
